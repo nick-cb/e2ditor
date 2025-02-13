263d3c979d86e9f15173895aa318f8eca6af7d7e
VI Nguyen <nguyenhungvicb@gmail.com>
2025-02-13 15:58:25 +0700

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,142 +1,31 @@
-"use client";
-
-import React, { startTransition, useCallback, useEffect, useState } from "react";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { QuestionSheet } from "@/components/QuestionSheet";
+import { Sheet, SheetTrigger } from "@/components/ui/sheet";
 import { CirclePlusIcon } from "lucide-react";
-import { QuestionForm, QuestionFormProvider, useQuestionForm } from "@/components/question-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { ControllerRenderProps, useForm } from "react-hook-form";
-import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
-import { getAllTest, saveTest } from "./actions";
-import { Form, FormField } from "@/components/ui/form";
+import React from "react";
+import { getAllTest } from "./actions";
+import Link from "next/link";
 
-const formSchema = z.object({
-  id: z.number().optional(),
-  testName: z.string(),
-  questions: z.array(
-    z.object({
-      id: z.number().optional(),
-      name: z.string(),
-      type: z.string(),
-      isDeleted: z.number(),
-      answers: z.array(
-        z.object({
-          id: z.number().optional(),
-          label: z.string().optional(),
-          content: z.string(),
-          isDeleted: z.number(),
-          isCorerct: z.number().default(0),
-        }),
-      ),
-      correctAnswer: z.number().optional(),
-      minAnswer: z.number().optional(),
-    }),
-  ),
-});
-export type QuestionFormSchema = z.infer<typeof formSchema>;
-export type QuestionFormType = ReturnType<typeof useForm<QuestionFormSchema>>;
-
-export default function Home() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      testName: "Untiled",
-      questions: [],
-    },
-  });
-  const questionForm = useQuestionForm();
-  function onSubmit(values: QuestionFormSchema) {
-    startTransition(async () => {
-      const newTest = await saveTest(values);
-    });
-  }
-
+export default async function Home() {
+  const tests = await getAllTest();
+  console.log(tests);
   return (
     <div className={"grid grid-cols-8 gap-2 p-3"}>
       <Sheet>
-        <SheetTrigger
-          className={
-            "aspect-square border-dashed border border-white/60 flex justify-center items-center gap-2"
-          }
-        >
+        <SheetTrigger className="aspect-square border-dashed border border-white/60 flex justify-center items-center gap-2">
           <CirclePlusIcon />
           <div>Add New</div>
         </SheetTrigger>
-        <SheetContent className={"!max-w-none w-1/2 p-0 overflow-y-scroll"}>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit, (err) => console.log(err))}>
-              <div className={"border-b min-h-10 p-4 flex items-center justify-between"}>
-                <div
-                  className={
-                    "items-center justify-center rounded-md bg-muted p-1 text-muted-foreground grid w-max grid-cols-2 gap-1"
-                  }
-                >
-                  <Button
-                    size="sm"
-                    variant={"outline"}
-                    className={cn(
-                      "hover:bg-background w-20",
-                      questionForm.mode === "normal" ? "bg-background" : "bg-muted",
-                    )}
-                    onClick={() => questionForm.changeMode("normal")}
-                  >
-                    Normal
-                  </Button>
-                  <Button
-                    size="sm"
-                    variant={"outline"}
-                    className={cn(
-                      "hover:bg-background w-20",
-                      questionForm.mode === "restore" ? "bg-background" : "bg-muted",
-                    )}
-                    onClick={() => questionForm.changeMode("restore")}
-                  >
-                    Restore
-                  </Button>
-                </div>
-                <Button variant={"outline"} type={"submit"} className={"mr-8"}>
-                  Save
-                </Button>
+        <QuestionSheet />
+        {tests.map((test) => {
+          return (
+            <Link href={`/?${test.id}`}>
+              <div className="aspect-square border border-white/60 flex justify-center items-center gap-2">
+                {test.name}
               </div>
-              <div className={"p-4"}>
-                <SheetHeader className={"my-4"}>
-                  <SheetTitle>
-                    <FormField
-                      control={form.control}
-                      name={"testName"}
-                      render={({ field }) => {
-                        return <TestName field={field} />;
-                      }}
-                    />
-                  </SheetTitle>
-                </SheetHeader>
-                <QuestionFormProvider questionForm={questionForm}>
-                  <QuestionForm form={form} />
-                </QuestionFormProvider>
-              </div>
-            </form>
-          </Form>
-        </SheetContent>
+            </Link>
+          );
+        })}
       </Sheet>
     </div>
   );
 }
-
-const TestName = React.memo(
-  ({ field }: { field: ControllerRenderProps<QuestionFormSchema, "testName"> }) => {
-    const [value] = useState(field.value);
-
-    return (
-      <div
-        contentEditable
-        onInput={(event) => field.onChange(event.currentTarget.textContent)}
-        suppressContentEditableWarning
-        className={"outline-none"}
-      >
-        {value}
-      </div>
-    );
-  },
-);
---
+++ components/QuestionSheet.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import React, { startTransition, useCallback, useEffect, useState } from "react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
+import { CirclePlusIcon } from "lucide-react";
+import { QuestionForm, QuestionFormProvider, useQuestionForm } from "@/components/question-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { ControllerRenderProps, useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { saveTest } from "../app/actions";
+import { Form, FormField } from "@/components/ui/form";
+
+const formSchema = z.object({
+  id: z.number().optional(),
+  testName: z.string(),
+  questions: z.array(
+    z.object({
+      id: z.number().optional(),
+      name: z.string(),
+      type: z.string(),
+      isDeleted: z.number(),
+      answers: z.array(
+        z.object({
+          id: z.number().optional(),
+          label: z.string().optional(),
+          content: z.string(),
+          isDeleted: z.number(),
+          isCorerct: z.number().default(0),
+        }),
+      ),
+      correctAnswer: z.number().optional(),
+      minAnswer: z.number().optional(),
+    }),
+  ),
+});
+export type QuestionFormSchema = z.infer<typeof formSchema>;
+export type QuestionFormType = ReturnType<typeof useForm<QuestionFormSchema>>;
+
+export function QuestionSheet() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      testName: "Untiled",
+      questions: [],
+    },
+  });
+  const questionForm = useQuestionForm();
+  function onSubmit(values: QuestionFormSchema) {
+    startTransition(async () => {
+      const newTest = await saveTest(values);
+    });
+  }
+  return (
+    <>
+      <SheetContent className={"!max-w-none w-1/2 p-0 overflow-y-scroll"}>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit, (err) => console.log(err))}>
+            <div className={"border-b min-h-10 p-4 flex items-center justify-between"}>
+              <div
+                className={
+                  "items-center justify-center rounded-md bg-muted p-1 text-muted-foreground grid w-max grid-cols-2 gap-1"
+                }
+              >
+                <Button
+                  size="sm"
+                  variant={"outline"}
+                  className={cn(
+                    "hover:bg-background w-20",
+                    questionForm.mode === "normal" ? "bg-background" : "bg-muted",
+                  )}
+                  onClick={() => questionForm.changeMode("normal")}
+                >
+                  Normal
+                </Button>
+                <Button
+                  size="sm"
+                  variant={"outline"}
+                  className={cn(
+                    "hover:bg-background w-20",
+                    questionForm.mode === "restore" ? "bg-background" : "bg-muted",
+                  )}
+                  onClick={() => questionForm.changeMode("restore")}
+                >
+                  Restore
+                </Button>
+              </div>
+              <Button variant={"outline"} type={"submit"} className={"mr-8"}>
+                Save
+              </Button>
+            </div>
+            <div className={"p-4"}>
+              <SheetHeader className={"my-4"}>
+                <SheetTitle>
+                  <FormField
+                    control={form.control}
+                    name={"testName"}
+                    render={({ field }) => {
+                      return <TestName field={field} />;
+                    }}
+                  />
+                </SheetTitle>
+              </SheetHeader>
+              <QuestionFormProvider questionForm={questionForm}>
+                <QuestionForm form={form} />
+              </QuestionFormProvider>
+            </div>
+          </form>
+        </Form>
+      </SheetContent>
+    </>
+  );
+}
+
+const TestName = React.memo(
+  ({ field }: { field: ControllerRenderProps<QuestionFormSchema, "testName"> }) => {
+    const [value] = useState(field.value);
+
+    return (
+      <div
+        contentEditable
+        onInput={(event) => field.onChange(event.currentTarget.textContent)}
+        suppressContentEditableWarning
+        className={"outline-none"}
+      >
+        {value}
+      </div>
+    );
+  },
+);
