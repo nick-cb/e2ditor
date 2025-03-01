7b7deb77afdfe351ca70754eba19e696d9b90d3b
VI Nguyen <nguyenhungvicb@gmail.com>
2025-03-01 10:08:43 +0700

    Add check loop

--- __tests__/blocks.test.tsx
+++ __tests__/blocks.test.tsx
@@ -1,16 +1,19 @@
-import { BlockList, createBlockList, IBlock, LineBlock } from "@/components/block";
+import { BlockList, createBlockList, IBlock, LineBlock, RootBlock } from "@/components/block";
 import { assert } from "@/lib/utils";
 import { test, expect } from "vitest";
 
 function createEditor() {
-  return { blocks: createBlockList<LineBlock>() };
+  return new RootBlock();
+  {
+    blocks: createBlockList<LineBlock>();
+  }
 }
 
 test("editor be able to add new item to the end of the list", () => {
   const editor = createEditor();
-  const newBlock = new LineBlock(null, null, null);
-  editor.blocks.addBlockToEnd(newBlock);
-  let blockArr = Array.from(editor.blocks);
+  const newBlock = editor.children.createBlock("block");
+  editor.children.addBlockToEnd(newBlock);
+  let blockArr = Array.from(editor.children);
   let firstBlock = blockArr[0];
   let lastBlock = blockArr.at(-1);
 
@@ -18,9 +21,9 @@ test("editor be able to add new item to the end of the list", () => {
   expect(lastBlock).toBe(newBlock);
   expect(lastBlock).toBe(firstBlock);
 
-  const newBlock2 = new LineBlock(null, null, null);
-  editor.blocks.addBlockToEnd(newBlock2);
-  blockArr = Array.from(editor.blocks);
+  const newBlock2 = editor.children.createBlock("block");
+  editor.children.addBlockToEnd(newBlock2);
+  blockArr = Array.from(editor.children);
   lastBlock = blockArr.at(-1);
   expect(blockArr.length).toBe(2);
   expect(lastBlock).toBe(newBlock2);
@@ -28,13 +31,15 @@ test("editor be able to add new item to the end of the list", () => {
   firstBlock = blockArr[0];
   expect(firstBlock.next).toBe(newBlock2);
   expect(newBlock2.prev).toBe(firstBlock);
+
+  checkLoop(editor);
 });
 
 test("editor be able to add new item to the start of the list", () => {
   const editor = createEditor();
-  const newBlock = new LineBlock(null, null, null);
-  editor.blocks.addBlockToStart(newBlock);
-  let blockArr = Array.from(editor.blocks);
+  const newBlock = editor.children.createBlock("block");
+  editor.children.addBlockToStart(newBlock);
+  let blockArr = Array.from(editor.children);
   let firstBlock = blockArr[0];
   let lastBlock = blockArr.at(-1);
 
@@ -42,9 +47,9 @@ test("editor be able to add new item to the start of the list", () => {
   expect(firstBlock).toBe(newBlock);
   expect(firstBlock).toBe(lastBlock);
 
-  const newBlock2 = new LineBlock(null, null, null);
-  editor.blocks.addBlockToStart(newBlock2);
-  blockArr = Array.from(editor.blocks);
+  const newBlock2 = editor.children.createBlock("block");
+  editor.children.addBlockToStart(newBlock2);
+  blockArr = Array.from(editor.children);
   firstBlock = blockArr[0];
   expect(blockArr.length).toBe(2);
   expect(firstBlock).toBe(newBlock2);
@@ -52,16 +57,18 @@ test("editor be able to add new item to the start of the list", () => {
   lastBlock = blockArr.at(-1);
   expect(newBlock2.next).toBe(lastBlock);
   expect(lastBlock?.prev).toBe(newBlock2);
+
+  checkLoop(editor);
 });
 
 test("editor be able to add new block as 'next' LineBlock for a block", () => {
   const editor = createEditor();
-  const newBlock = new LineBlock(null, null, null);
-  editor.blocks.addBlockToStart(newBlock);
+  const newBlock = editor.children.createBlock("block");
+  editor.children.addBlockToStart(newBlock);
 
-  const newBlock2 = new LineBlock(null, null, null);
-  editor.blocks.insertBlockAfter(newBlock, newBlock2);
-  const blockArr = Array.from(editor.blocks);
+  const newBlock2 = editor.children.createBlock("block");
+  editor.children.insertBlockAfter(newBlock, newBlock2);
+  const blockArr = Array.from(editor.children);
   const lastBlock = blockArr.at(-1);
   expect(blockArr.length).toBe(2);
   expect(lastBlock).toBe(newBlock2);
@@ -69,32 +76,46 @@ test("editor be able to add new block as 'next' LineBlock for a block", () => {
   const firstBlock = blockArr[0];
   expect(firstBlock.next).toBe(newBlock2);
   expect(newBlock2.prev).toBe(firstBlock);
+
+  checkLoop(editor);
 });
 
 test("editor be able to delete a block", () => {
   const editor = createEditor();
-  const newBlock = new LineBlock(null, null, null);
-  editor.blocks.addBlockToStart(newBlock);
-  let blockArr = Array.from(editor.blocks);
+  const newBlock = editor.children.createBlock("block");
+  editor.children.addBlockToStart(newBlock);
+  let blockArr = Array.from(editor.children);
   expect(blockArr.length).toBe(1);
 
-  editor.blocks.deleteBlock(newBlock);
-  blockArr = Array.from(editor.blocks);
+  editor.children.deleteBlock(newBlock);
+  blockArr = Array.from(editor.children);
   expect(blockArr.length).toBe(0);
+
+  checkLoop(editor);
 });
 
 test("move a block as the child of another block", () => {
   const editor = createEditor();
-  const newBlock = new LineBlock(null, null, null);
-  editor.blocks.addBlockToStart(newBlock);
-  const newBlock2 = new LineBlock(null, null, null);
-  editor.blocks.addBlockToEnd(newBlock2);
-  expect(Array.from(editor.blocks).length).toBe(2);
+  const newBlock = editor.children.createBlock("block");
+  editor.children.addBlockToStart(newBlock);
+  const newBlock2 = editor.children.createBlock("block");
+  editor.children.addBlockToEnd(newBlock2);
+  expect(Array.from(editor.children).length).toBe(2);
 
-  const block2 = editor.blocks.deleteBlock(newBlock2);
-  expect(Array.from(editor.blocks).length).toBe(1);
+  const block2 = editor.children.deleteBlock(newBlock2);
+  expect(Array.from(editor.children).length).toBe(1);
 
   newBlock.children.addBlockToEnd(block2);
   expect(Array.from(newBlock.children).length).toBe(1);
   expect(Array.from(newBlock.children)[0]).toBe(block2);
+
+  checkLoop(editor);
 });
+
+function checkLoop(root: RootBlock) {
+  const visited = new Map<string, number>();
+  for (const child of root.children) {
+    if (visited.get(child.id) === 1) throw new Error("Detected loop");
+    else visited.set(child.id, 1);
+  }
+}
